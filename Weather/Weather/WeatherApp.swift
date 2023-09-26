6397ee09fc95ad7c7c31fbee3f703e0c356f8a26
dante canizo <dantecanizo@dantes-MacBook-Pro-2.local>
2023-09-25 21:54:37 -0300

    Starting project

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by dante canizo on 25/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
